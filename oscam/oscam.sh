c88e33a23ce14cb7f5a7b82514038408baf95e11
tobbenb <torbjornbrekke@gmail.com>
2015-01-10 22:44:58 +0100

    Changed back to running oscam as nobody

--- oscam/oscam.sh
+++ oscam/oscam.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-#exec /sbin/setuser nobody /usr/bin/oscam-svn -c /config
-exec /usr/bin/oscam-svn -c /config
+exec /sbin/setuser nobody /usr/bin/oscam-svn -c /config
+#exec /usr/bin/oscam-svn -c /config
