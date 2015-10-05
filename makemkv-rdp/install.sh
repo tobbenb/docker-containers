75d415a797476feea5993d8b63c1d42200a68725
tobbenb <tobbenb@users.noreply.github.com>
2015-10-05 23:28:51 +0200

    Update install.sh

--- makemkv-rdp/install.sh
+++ makemkv-rdp/install.sh
@@ -7,8 +7,8 @@
 #									#
 #####################################
 
-apt-get update
-apt-get install build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev wget
+apt-get update -qq
+apt-get install -qy build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev wget
 
 #####################################
 #	Download sources and extract	#
@@ -19,11 +19,13 @@ mkdir -p /tmp/sources
 wget -O /tmp/sources/makemkv-bin-1.9.7.tar.gz http://www.makemkv.com/download/makemkv-bin-1.9.7.tar.gz
 wget -O /tmp/sources/makemkv-oss-1.9.7.tar.gz http://www.makemkv.com/download/makemkv-oss-1.9.7.tar.gz
 wget -O /tmp/sources/ffmpeg-2.8.tar.bz2 https://ffmpeg.org/releases/ffmpeg-2.8.tar.bz2
+pushd /tmp/sources/
 tar xvzf /tmp/sources/makemkv-bin-1.9.7.tar.gz
 tar xvzf /tmp/sources/makemkv-oss-1.9.7.tar.gz
 tar xvjf /tmp/sources/ffmpeg-2.8.tar.bz2
 cp /tmp/ask_eula.sh /tmp/sources/makemkv-bin-1.9.7/src/ 
 cp /tmp/Makefile /tmp/sources/makemkv-bin-1.9.7/
+popd
 
 #####################################
 #	Compile and install				#
@@ -34,14 +36,19 @@ cp /tmp/Makefile /tmp/sources/makemkv-bin-1.9.7/
 pushd /tmp/sources/ffmpeg-2.8
 ./configure --prefix=/tmp/ffmpeg --enable-static --disable-shared --enable-pic --disable-yasm
 make install
+popd
 
 #Makemkv-oss
+pushd /tmp/sources/makemkv-oss-1.9.7
 PKG_CONFIG_PATH=/tmp/ffmpeg/lib/pkgconfig ./configure
 make
 make install
+popd
 
 #Makemkv-bin
+pushd /tmp/sources/makemkv-bin-1.9.7
 make install
+popd
 
 #####################################
 #	Remove unneeded packages		#
