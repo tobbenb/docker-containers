1b240bbfb7b1ce04fe700432aaa3081ec516163d
tobbenb <torbjornbrekke@gmail.com>
2016-01-19 22:09:18 +0100

    Fixed norwegian keymap for xrdp

--- makemkv-rdp/files/install/install.sh
+++ makemkv-rdp/files/install/install.sh
@@ -14,7 +14,7 @@ apt-get install -qy build-essential pkg-config libc6-dev libssl-dev libexpat1-de
 #	Download sources and extract	#
 #									#
 #####################################
-VERSION="1.9.8"
+VERSION="1.9.9"
 
 mkdir -p /tmp/sources
 wget -O /tmp/sources/makemkv-bin-$VERSION.tar.gz http://www.makemkv.com/download/makemkv-bin-$VERSION.tar.gz
@@ -24,8 +24,8 @@ pushd /tmp/sources/
 tar xvzf /tmp/sources/makemkv-bin-$VERSION.tar.gz
 tar xvzf /tmp/sources/makemkv-oss-$VERSION.tar.gz
 tar xvjf /tmp/sources/ffmpeg-2.8.tar.bz2
-cp /tmp/ask_eula.sh /tmp/sources/makemkv-bin-$VERSION/src/ 
-cp /tmp/Makefile /tmp/sources/makemkv-bin-$VERSION/
+cp /tmp/install/ask_eula.sh /tmp/sources/makemkv-bin-$VERSION/src/ 
+cp /tmp/install/Makefile /tmp/sources/makemkv-bin-$VERSION/
 popd
 
 #####################################
@@ -51,12 +51,30 @@ pushd /tmp/sources/makemkv-bin-$VERSION
 make install
 popd
 
+
+#####################################
+#	Fix keyboard mappings rdp		#
+#									#
+#####################################
+sed -i.bak '/[default_rdp_layouts]/ a rdp_layout_no=0x00000414' /etc/xrdp/xrdp_keyboard.ini
+sed -i.bak '/[default_layouts_map]/ a rdp_layout_no=no' /etc/xrdp/xrdp_keyboard.ini
+cp /tmp/install/keymaps/*.ini /etc/xrdp/
+
+#####################################
+#	Add configs and needed stuff	#
+#									#
+#####################################
+cp /tmp/install/startapp.sh /startapp.sh
+chmod +x /startapp.sh
+cp /tmp/install/firstrun.sh /etc/my_init.d/firstrun.sh
+chmod +x /etc/my_init.d/firstrun.sh
+
 #####################################
 #	Remove unneeded packages		#
 #									#
 #####################################
 
 apt-get remove -qy build-essential pkg-config libc6-dev libssl-dev libexpat1-dev libavcodec-dev libgl1-mesa-dev libqt4-dev
-apt-get clean && rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
+apt-get clean && rm -rf /var/lib/apt/lists/* /var/tmp/*
 
 exit
