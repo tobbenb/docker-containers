7e84c7d32c4967b378ae97ec71f8106f2c4de46a
tobbenb <tobbenb@users.noreply.github.com>
2016-07-22 20:56:19 +0200

    Remove lock file if exists

--- makemkv-rdp/files/install/firstrun.sh
+++ makemkv-rdp/files/install/firstrun.sh
@@ -9,3 +9,8 @@ chmod -R g+rw /config
 #Add nobody to cdrom group
 addgroup --gid 19 makemkv
 usermod -a -G 19 nobody
+
+#Remove Lock file if it exists.
+if [ ! -f /tmp/.X1-lock]; then
+  rm /tmp/.X1-lock
+fi
