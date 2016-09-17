f4b7ee50e6c46ca04992374393f16d59e535113f
tobbenb <tobbenb@users.noreply.github.com>
2016-09-17 19:05:00 +0200

    Update tvheadend to lastest build

--- tvheadend-unstable/files/install/install.sh
+++ tvheadend-unstable/files/install/install.sh
@@ -35,7 +35,7 @@ pushd /tmp/
 git clone https://github.com/tvheadend/tvheadend.git 
 popd
 pushd /tmp/tvheadend
-git checkout f34fac1a 
+git checkout 817f67e7 
 ./configure --enable-libffmpeg_static
 make 
 make install
